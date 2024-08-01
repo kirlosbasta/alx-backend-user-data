c52f6e5b52fd5e004a9a5eaecd83b674990b81a5
kirlos basta <kirlosbasta@gmail.com>
2024-08-01 19:28:47 +0300

    Add is_valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ def hash_password(password: str) -> bytes:
     '''Hash a pasword using bcrypt'''
     return bcrypt.hashpw(bytes(password, encoding='utf-8'),
                          salt=bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''check if password matches the hashed password'''
+    return bcrypt.checkpw(bytes(password, encoding='utf-8'), hashed_password)
