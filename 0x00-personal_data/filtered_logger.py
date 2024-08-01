2c1e2e5ec358f8a85a94784097b2f0901caba72d
kirlos basta <kirlosbasta@gmail.com>
2024-08-01 03:33:50 +0300

    Add get_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import logging
 from typing import List
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
