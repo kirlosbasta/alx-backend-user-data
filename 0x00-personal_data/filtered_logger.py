cff4f453ccf802858159cb3dbff9cc654ad74408
kirlos basta <kirlosbasta@gmail.com>
2024-08-01 03:14:02 +0300

    Add formater

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''0. Regex-ing'''
 import re
+import logging
 from typing import List
 
 
@@ -13,3 +14,25 @@ def filter_datum(fields: List[str], redaction: str,
                                f'{fields[i]}={redaction}',
                                data) for data in message_data]
     return separator.join(message_data)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor method
+            """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format method
+            """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)        
+
