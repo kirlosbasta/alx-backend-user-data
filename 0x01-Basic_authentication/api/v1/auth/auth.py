5f86338e3cb47a26487149457c1bab32cb0dd04c
kirlos basta <kirlosbasta@gmail.com>
2024-08-05 20:29:38 +0300

    Add require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,13 @@ class Auth():
     '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''pass to later'''
-        return False
+        if path is None or not excluded_paths:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''pass to later'''
