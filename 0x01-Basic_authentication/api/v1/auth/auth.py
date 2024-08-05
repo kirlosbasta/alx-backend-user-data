28128e18ffb161dc544e838cfc35f25220ab943c
kirlos basta <kirlosbasta@gmail.com>
2024-08-05 22:05:28 +0300

    Add request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,9 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         '''pass to later'''
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''pass to later'''
