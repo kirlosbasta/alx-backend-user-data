170c190706c71f71c9eee878bb4f5f741fa0a6e6
kirlos basta <kirlosbasta@gmail.com>
2024-08-06 23:04:13 +0300

    Add advanced tasks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,9 +20,9 @@ class Auth():
             if excluded_path[-1] == '*':
                 pattern = '{}.*'.format(excluded_path[0:-1])
             elif excluded_path[-1] == '/':
-                pattern = '{}/*'.format(excluded_path[0:-1])
+                pattern = excluded_path
             else:
-                pattern = '{}/*'.format(excluded_path)
+                pattern = excluded_path
             if re.search(pattern, path) is not None:
                 return False
         return True
