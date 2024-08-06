b6de5dbd71dd522225476b5815e9d2a3ec7524b7
kirlos basta <kirlosbasta@gmail.com>
2024-08-06 19:31:19 +0300

    Add extract_user_data

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 '''
 from api.v1.auth.auth import Auth
 from flask import request
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,26 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        '''decode base64 authorization header to a utf-8 string'''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return (base64.b64decode(s=base64_authorization_header, validate=True)).decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        '''separate username and password from decoded_base64_authorization_header'''
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        username, password = decoded_base64_authorization_header.split(':', maxsplit=1)
+        return username, password
