dd784b08015708fff68050a4cfcd87864e30ee34
kirlos basta <kirlosbasta@gmail.com>
2024-08-07 23:42:18 +0300

    Add login route

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,22 +8,21 @@ from os import getenv
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    ''''''
+    '''login the user to the session'''
     email = request.form.get('email')
     password = request.form.get('password')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     users = User.search({'email': email})
     if len(users) == 0:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user = users[0]
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     resp = make_response(jsonify(user.to_json()))
     resp.set_cookie(getenv('SESSION_NAME'), session_id)
     return resp
-    
