c81f711dae6c678efe30d0cb03d2f2d5f754a3e1
kirlos basta <kirlosbasta@gmail.com>
2024-08-07 20:34:37 +0300

    Add create_session method

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+class SessionAuth that inherits from Auth
+'''
+from api.v1.auth.auth import Auth
+from flask import request
+import base64
+from typing import TypeVar, Dict
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    '''Session base authentication class'''
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Create Session ID for user_id'''
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+        
