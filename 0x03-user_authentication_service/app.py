132cdd8ee7b65e6f4df1fec25da42f734baf68f9
Kirlos Basta <kirlosbasta@gmail.com>
2024-08-14 00:21:29 +0300

    add user route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 '''App Dirver Module'''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def bienvenue() -> str:
     '''return a simple message'''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    '''Regestir a user'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
