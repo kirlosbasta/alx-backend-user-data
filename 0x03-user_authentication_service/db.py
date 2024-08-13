10f3f6416686af259f541bc6347bc5d5194cc6ec
Kirlos Basta <kirlosbasta@gmail.com>
2024-08-13 16:55:31 +0300

    Add find_userby

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,8 +4,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.session import Session
-
+from typing import List, Dict
 from user import Base, User
 
 
@@ -35,4 +37,14 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, *args: List, **kwargs: Dict) -> User:
+        '''return the first user match kwargs'''
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError:
+            raise InvalidRequestError
+        if not user:
+            raise NoResultFound
         return user
