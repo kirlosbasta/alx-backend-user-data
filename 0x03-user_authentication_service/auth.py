003922415a04534ef8db83397bdaf7d4a6cd9aad
Kirlos Basta <kirlosbasta@gmail.com>
2024-08-14 19:52:58 +0300

    add get_reset_password_token method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,13 @@ class Auth:
         except Exception:
             return None
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''Generate and return reset password token'''
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
