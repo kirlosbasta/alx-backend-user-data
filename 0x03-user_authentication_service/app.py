9d692fc9d8ef5bac32a7338fadf03ec837db451c
Kirlos Basta <kirlosbasta@gmail.com>
2024-08-14 14:56:00 +0300

    add login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 '''App Dirver Module'''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/', methods=['GET'])
 def bienvenue() -> str:
     '''return a simple message'''
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'], strict_slashes=False)
+@app.route('/users', methods=['POST'])
 def users() -> str:
     '''Regestir a user'''
     email = request.form.get('email')
@@ -24,6 +25,19 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    '''Manages login'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    res = make_response(jsonify({"email": email, "message": "logged in"}))
+    res.set_cookie("session_id", session_id)
+    return res
 
 
 if __name__ == "__main__":
